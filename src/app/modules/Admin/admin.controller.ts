ac2a513c1ba4ebec437d39aaa742c36d6cef3f3a
oliullah05 <mdolihasan@gmail.com>
2024-03-17 14:55:39 +0600

    Implementing Search/Filter Functionality Using SearchTerm Query

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,16 +2,21 @@ import { Request, Response } from "express";
 import { adminServices } from "./admin.service";
 
 const getAllAdmin = async (req: Request, res: Response) => {
+ 
     try {
-        const result = await adminServices.getAllAdmin();
+        const result = await adminServices.getAllAdmin(req.query);
         res.status(200).json({
             success: true,
             message: "Admins are retrieve successfully",
             data: result
         })
     }
-    catch (err) {
-
+    catch (err:any) {
+        res.status(500).json({
+            success:false,
+            message:err.name  || "Something went wrong",
+            error:err
+           })
     }
 }
 
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,8 +2,33 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient();
 
-const getAllAdmin = async()=>{
-const result = await prisma.admin.findMany()
+const getAllAdmin = async(params:Record<string,any>)=>{
+const result = await prisma.admin.findMany({
+    // where:{
+    //     name:{
+    //         contains:params.searchTerm,
+    //         mode:"insensitive"
+    //     }
+    // }
+
+
+    where:{
+        OR:[
+            {
+                name:{
+                            contains:params.searchTerm,
+                            mode:"insensitive"
+                        }
+            },
+            {
+                email:{
+                    contains:params.searchTerm,
+                    mode:"insensitive"
+                }
+            }
+        ]
+    }
+})
 return result
 }
 
