2879b8e61dd8d26531083fcb5823a1e3cc507d0f
oliullah05 <mdolihasan@gmail.com>
2024-03-18 17:44:36 +0600

    Implementing Filtering On Specific Fields And Values

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,15 +3,11 @@ import { Prisma, PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient();
 
 const getAllAdmin = async(params:Record<string,any>)=>{
+    const {searchTerm,...filterData}= params;
 const andConditions:Prisma.AdminWhereInput[] = [];
 
+console.log({filterData});
 const adminSearchAbleFields = ["name","email"]
-
-
-
-
-
-
 
 if(params.searchTerm){
     andConditions.push({
@@ -24,13 +20,26 @@ if(params.searchTerm){
     })
 }
 
+
+if(Object.keys(filterData).length>0){
+    andConditions.push({
+        AND:Object.keys(filterData).map(key=>({
+         [key]:{
+            equals:filterData[key]
+         }
+        }))
+    })
+}
+
+
+
+
+
+
+
 const whereConditions:Prisma.AdminWhereInput = {AND:andConditions}
-
-
-console.dir(andConditions,{depth:"infinity"});
+// console.dir(andConditions,{depth:"infinity"});
 const result = await prisma.admin.findMany({
-
-
     where:whereConditions
 })
 return result
