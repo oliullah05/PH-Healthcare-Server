daed15c5b4adc0acd9a1ec66f958faa53f413899
oliullah05 <mdolihasan@gmail.com>
2024-03-22 19:17:57 +0600

    28-3 Implementing Request Validation Middleware: Part 2

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,12 +1,31 @@
 import express, { NextFunction, Request, Response } from "express"
 import { adminControllers } from "./admin.controller";
+import { AnyZodObject, z } from "zod";
 
 const router = express.Router();
 
 
-const validateRequest = (req:Request,res:Response,next:NextFunction)=>{
-    console.log("middlewars",req.body);
-    next()
+const update = z.object({
+    body:z.object({
+        name:z.string().optional(),
+        contactNumber:z.string().optional()
+
+    })
+})
+
+
+const validateRequest = (schema:AnyZodObject)=>{
+ return  async (req:Request,res:Response,next:NextFunction)=>{
+    try{
+        await schema.parseAsync({
+            body:req.body
+        })
+      return  next()
+    }
+    catch(err){
+        next(err)
+    }
+    }
 }
 
 
@@ -15,7 +34,7 @@ const validateRequest = (req:Request,res:Response,next:NextFunction)=>{
 
 router.get("/",adminControllers.getAllAdmin)
 router.get("/:id",adminControllers.getSingleAdminById)
-router.patch("/:id",validateRequest,adminControllers.updateSingleAdmin)
+router.patch("/:id",validateRequest(update),adminControllers.updateSingleAdmin)
 router.delete("/:id",adminControllers.deleteSingleAdmin)
 router.delete("/soft/:id",adminControllers.softDeleteSingleAdmin)
 
