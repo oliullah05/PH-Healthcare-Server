fd2d63b9ffd05aae1da4e9e4287832ef8525e324
oliullah05 <mdolihasan@gmail.com>
2024-03-22 17:35:39 +0600

    Reduce Try-Catch Redundancy

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,114 +1,94 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { adminServices } from "./admin.service";
-import  pick  from "../../../shared/pick";
+import pick from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.const";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 
-
-
-const getAllAdmin = async (req: Request, res: Response,next:NextFunction) => {
- const filters = pick(req.query,adminFilterableFields);
- const options = pick(req.query,['page',"limit","sortBy","sortOrder"]);
-    try {
-        const result = await adminServices.getAllAdmin(filters,options);
-        // res.status(200).json({
-        //     success: true,
-        //     message: "Admins are retrieve successfully",
-        //     meta:result.meta,
-        //     data: result.data
-        // })
-        sendResponse(res,{
-            success:true,
-            message: "Admins are retrieve successfully",
-            statusCode:200,
-            meta:result.meta,
-            data: result.data
-        })
-    }
-    catch (err:any) {
-       next(err)
+const catchAsync = (fn: RequestHandler) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await fn(req, res, next)
+        }
+        catch (err) {
+            next(err)
+        }
     }
 }
 
+const getAllAdmin: RequestHandler = catchAsync(async (req, res) => {
+
+    const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ['page', "limit", "sortBy", "sortOrder"]);
+    const result = await adminServices.getAllAdmin(filters, options);
+    sendResponse(res, {
+        success: true,
+        message: "Admins are retrieve successfully",
+        statusCode: 200,
+        meta: result.meta,
+        data: result.data
+    })
+
+})
 
 
-const getSingleAdminById = async (req: Request, res: Response,next:NextFunction) => {
+
+const getSingleAdminById: RequestHandler = catchAsync(async (req, res) => {
     const id = req.params.id
-    try {
-           const result = await adminServices.getSingleAdminById(id)
-           sendResponse(res,{
-            success: true,
-            message: "Admin is retrieve successfully",
-            data: result,
-            statusCode:httpStatus.OK
-        })
-       }
-       catch (err:any) {
-        next(err)
-       }
-   }
-   
+    const result = await adminServices.getSingleAdminById(id)
+    sendResponse(res, {
+        success: true,
+        message: "Admin is retrieve successfully",
+        data: result,
+        statusCode: httpStatus.OK
+    })
+}
+)
 
 
 
 
-   const updateSingleAdmin = async(req: Request, res: Response,next:NextFunction)=>{
+const updateSingleAdmin: RequestHandler = catchAsync(async (req, res) => {
     const id = req.params.id;
     const updatedData = req.body;
-    try {
-           const result = await adminServices.updateSingleAdmin(id,updatedData)
-           sendResponse(res,{
-            success: true,
-            message: "Admin is updated successfully",
-            data: result,
-            statusCode:httpStatus.OK
-        })
-       }
-       catch (err:any) {
-        next(err)
-       }
-   }
+    const result = await adminServices.updateSingleAdmin(id, updatedData)
+    sendResponse(res, {
+        success: true,
+        message: "Admin is updated successfully",
+        data: result,
+        statusCode: httpStatus.OK
+    })
+
+})
 
 
-   const deleteSingleAdmin= async(req: Request, res: Response,next:NextFunction)=>{
+const deleteSingleAdmin: RequestHandler = catchAsync(async (req, res) => {
     const id = req.params.id;
-       try {
-        const result = await adminServices.deleteSingleAdmin(id)
-
-           sendResponse(res,{
-            success: true,
-            message: "Admin is deleted successfully",
-            data: result,
-            statusCode:httpStatus.OK
-        })
-       }
-       catch (err:any) {
-        next(err)
-       }
-   }
+    const result = await adminServices.deleteSingleAdmin(id)
+    sendResponse(res, {
+        success: true,
+        message: "Admin is deleted successfully",
+        data: result,
+        statusCode: httpStatus.OK
+    })
+}
+)
 
 
 
-
-   const softDeleteSingleAdmin= async(req: Request, res: Response,next:NextFunction)=>{
+const softDeleteSingleAdmin: RequestHandler = catchAsync(async (req, res) => {
     const id = req.params.id;
-   
-    
-       try {
-        const result = await adminServices.softDeleteSingleAdmin(id)
-           res.status(200).json()
-           sendResponse(res,{
-            success: true,
-            message: "Admin is deleted successfully",
-            data: result,
-            statusCode:httpStatus.OK
-        })
-       }
-       catch (err:any) {
-         next(err)
-       }
-   }
+
+    const result = await adminServices.softDeleteSingleAdmin(id)
+    res.status(200).json()
+    sendResponse(res, {
+        success: true,
+        message: "Admin is deleted successfully",
+        data: result,
+        statusCode: httpStatus.OK
+    })
+
+})
 
 
 
