f0be578e2d8527f3a3bc2fdbbb62760e7501eb7b
oliullah05 <mdolihasan@gmail.com>
2024-03-23 20:16:21 +0600

    Implementing Authentication Middleware For Role-Based Access On APIs

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,9 +1,30 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 import { userController } from "./user.controller";
+import { jwtHelpers } from "../../../hepers/jwtHelpers";
+import config from "../../../config";
+import { Secret } from "jsonwebtoken";
 
 const router = express.Router();
-
-router.post("/",userController.createAdmin)
+const auth = (...roles: string[]) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const accessToken = req.headers.authorization
+           if(!accessToken){
+            throw new Error("you are not authorized")
+           }
+           const verifyToken =await jwtHelpers.verifyToken(accessToken,config.jwt.jwt_access_secret as string);
+           
+           if(roles.length && !roles.includes(verifyToken.role)){
+            throw new Error("you are not authorized")
+           }
+           next()
+        }
+        catch (err) {
+            next(err)
+        }
+    }
+}
+router.post("/", auth("ADMIN", "SUPER_ADMIN"), userController.createAdmin)
 
 export const userRoutes = router;
 
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { Server } from "https";
 import app from "./app"
+import config from "./config";
 const port = 3000;
 
 async function main() {
    
-    const server:Server = app.listen(port,()=>{
-        console.log(`PH Healthcare is running on port ${port}`);
+    const server:Server = app.listen(config.port,()=>{
+        console.log(`PH Healthcare is running on port ${config.port}`);
     })
 }
 
