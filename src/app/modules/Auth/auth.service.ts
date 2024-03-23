8d4f2fa75b99e2d349aa5596a6bf45a1dd62733f
oliullah05 <mdolihasan@gmail.com>
2024-03-23 13:18:00 +0600

    Generating Access Tokens For User Login

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,5 +1,26 @@
+import prisma from "../../../shared/prisma"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const loginUser = async(payload:{email:string,password:string})=>{
-    console.log("log in",payload);
+    const userData =await prisma.user.findUniqueOrThrow({
+        where:{
+            email:payload.email
+        }
+    })
+
+
+const isCorrectPassword:boolean =await bcrypt.compare(payload.password, userData.password);
+
+const accessToken = jwt.sign({email:userData.email,role:userData.role},"secretKRy",{
+    expiresIn:"10m",
+    algorithm:"HS256"
+})
+
+console.log({accessToken});
+return userData
+
+
+
 }
 
 
