d57335c01d8bf3364f3df92665d2926400e9860b
oliullah05 <mdolihasan@gmail.com>
2024-03-24 16:38:28 +0600

    Implementing Role-Based Access Control On API Routes

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -3,6 +3,8 @@ import { adminControllers } from "./admin.controller";
 
 import validateRequest from "../../middlewars/validateRequest";
 import { adminValidationsSchemas } from "./admin.validation";
+import auth from "../../middlewars/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
@@ -15,11 +17,32 @@ const router = express.Router();
 
 
 
-router.get("/",adminControllers.getAllAdmin)
-router.get("/:id",adminControllers.getSingleAdminById)
-router.patch("/:id",validateRequest(adminValidationsSchemas.updateAdmin),adminControllers.updateSingleAdmin)
-router.delete("/:id",adminControllers.deleteSingleAdmin)
-router.delete("/soft/:id",adminControllers.softDeleteSingleAdmin)
+router.get(
+    "/",
+auth(UserRole.SUPER_ADMIN,UserRole.ADMIN),
+ adminControllers.getAllAdmin
+ )
+router.get("/:id",
+auth(UserRole.SUPER_ADMIN,UserRole.ADMIN),
+adminControllers.getSingleAdminById
+)
+
+
+router.patch("/:id",
+auth(UserRole.SUPER_ADMIN,UserRole.ADMIN),
+validateRequest(adminValidationsSchemas.updateAdmin),
+adminControllers.updateSingleAdmin
+)
+
+router.delete("/:id",
+auth(UserRole.SUPER_ADMIN,UserRole.ADMIN),
+adminControllers.deleteSingleAdmin
+)
+
+router.delete("/soft/:id",
+auth(UserRole.SUPER_ADMIN,UserRole.ADMIN),
+adminControllers.softDeleteSingleAdmin
+)
 
 
 export const adminRoutes = router;
--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import auth from "../../middlewars/auth";
 import { userController } from "./user.controller";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
-router.post("/", auth("ADMIN", "SUPER_ADMIN"), userController.createAdmin)
+router.post("/", auth(UserRole.ADMIN,UserRole.SUPER_ADMIN), userController.createAdmin)
 
 export const userRoutes = router;
 
