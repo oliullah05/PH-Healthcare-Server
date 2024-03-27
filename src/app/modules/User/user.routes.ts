5bcaa29fa8da1f8c060d184c7913f0f551ed9ee8
oliullah05 <mdolihasan@gmail.com>
2024-03-27 16:37:27 +0600

    Creating Prisma Schema For Doctor

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -7,11 +7,15 @@ import { userValidation } from "./user.validition";
 
 const router = express.Router();
 
-
-
-
-
-router.post("/", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+router.post("/create-admin", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  fileUploader.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data))
+   
+    next()
+  }, userController.createAdmin
+)
+router.post("/create-doctor", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
   fileUploader.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = userValidation.createAdmin.parse(JSON.parse(req.body.data))
