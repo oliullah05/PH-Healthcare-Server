938097662a1b0af841cc08415acb89bbb7ad4b7b
oliullah05 <mdolihasan@gmail.com>
2024-03-27 12:50:09 +0600

    30-1 Uploading Images To Current Working Directory With Multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,11 +1,20 @@
+import { UserRole } from "@prisma/client";
 import express from "express";
+import { fileUploader } from "../../../hepers/fileUploader";
 import auth from "../../middlewars/auth";
 import { userController } from "./user.controller";
-import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 
-router.post("/", auth(UserRole.ADMIN,UserRole.SUPER_ADMIN), userController.createAdmin)
+
+
+
+
+
+router.post("/", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+   fileUploader.upload.single("file"),
+    userController.createAdmin
+)
 
 export const userRoutes = router;
 
