7f4cbafe36dae6486fd9b73d4f17b4dc5e4900b7
oliullah05 <mdolihasan@gmail.com>
2024-04-15 18:16:33 +0600

    Creating Doctor Specialties

--- src/app/modules/Doctor/doctor.service.ts
+++ src/app/modules/Doctor/doctor.service.ts
@@ -1,21 +1,43 @@
 import { Doctor } from "@prisma/client";
 import prisma from "../../../shared/prisma";
+import { date } from "zod";
 
-const updateDoctor = async(id:string,payload:Doctor)=>{
-const doctorData = await prisma.doctor.findUniqueOrThrow({
+const updateDoctor = async(id:string,payload:Doctor & {specialties:string[]})=>{
+const {specialties,...doctorData} = payload
+const doctorInfo = await prisma.doctor.findUniqueOrThrow({
     where:{
         id
     }
 })
 
-const updateDoctorData = prisma.doctor.update({
-    where:{
-        id
-    },
-    data:payload
+
+const result = await prisma.$transaction(async(transactionClient)=>{
+
+    const updateDoctorData = transactionClient.doctor.update({
+        where:{
+            id
+        },
+        data:doctorData
+    })
+
+
+const doctorSpecialities= specialties.map((specialties)=>{
+    return {
+        doctorId:doctorInfo.id,
+        specialtiesId:specialties
+    }
 })
 
-return updateDoctorData
+  const createDoctorSpecialities = await transactionClient.doctorSpecialties.createMany({
+    data:doctorSpecialities
+  })
+  return updateDoctorData
+})
+
+
+
+
+return result
 }
 
 
