8082a8b3bd28b1d79d79e69090fb835d91410a79
oliullah05 <mdolihasan@gmail.com>
2024-04-15 21:24:58 +0600

    Updating Doctor Specialties: Part 2

--- src/app/modules/Doctor/doctor.service.ts
+++ src/app/modules/Doctor/doctor.service.ts
@@ -1,72 +1,84 @@
 import { Doctor, Prisma } from "@prisma/client";
 import prisma from "../../../shared/prisma";
 
-const updateDoctor = async(id:string,payload:Doctor & {specialties:[{specialtiesId:string,isDeleted:boolean}]})=>{
-const {specialties,...doctorData} = payload;
-const doctorInfo = await prisma.doctor.findUniqueOrThrow({
-    where:{
-        id
-    }
-})
-
-
-
-const result =await  prisma.$transaction(async(transactionClient)=>{
-
-    const updateDoctorData = transactionClient.doctor.update({
-        where:{
+const updateDoctor = async (id: string, payload: Doctor & { specialties: [{ specialtiesId: string, isDeleted: boolean }] }) => {
+    const { specialties, ...doctorData } = payload;
+    const doctorInfo = await prisma.doctor.findUniqueOrThrow({
+        where: {
             id
-        },
-        data:doctorData
+        }
+    })
+
+
+    const result = await prisma.$transaction(async (transactionClient) => {
+        //    updateDoctorData 
+        transactionClient.doctor.update({
+            where: {
+                id
+            },
+            data: doctorData
+        })
+
+
+
+        let deleteSpacialtiesIds: string[] = [];
+        let createSpacialtiesIds: string[] = [];
+        if (specialties && specialties.length > 0) {
+            specialties.filter(s => {
+                if (s.isDeleted === true) {
+                    deleteSpacialtiesIds.push(s.specialtiesId)
+                }
+                if (s.isDeleted === false) {
+                    createSpacialtiesIds.push(s.specialtiesId)
+                }
+            })
+        }
+
+
+
+        const doctorSpecialitiesCreateData = createSpacialtiesIds.map((specialties) => {
+            return {
+                doctorId: doctorInfo.id,
+                specialtiesId: specialties
+            }
+        })
+
+        // createDoctorSpecialities
+        await transactionClient.doctorSpecialties.createMany({
+            data: doctorSpecialitiesCreateData
+        })
+
+        //   deleteDoctorSpecialities
+        await transactionClient.doctorSpecialties.deleteMany({
+            where: {
+                specialtiesId: {
+                    in: deleteSpacialtiesIds
+                }
+            }
+        })
+
+
+
+        const result = await transactionClient.doctor.findUnique({
+            where: {
+                id: doctorInfo.id
+            },
+            include: {
+                doctorSpecialties: {
+                    include: {
+                        specialties: true
+                    }
+                }
+            }
+        })
+
+        return result
     })
 
 
 
-    let deleteSpacialtiesIds:string[] =[];
-    let createSpacialtiesIds:string[] =[];
-    if(specialties && specialties.length>0){ 
-        specialties.filter(s=>{
-            if(s.isDeleted===true) {
-                deleteSpacialtiesIds.push(s.specialtiesId)
-            }
-            if(s.isDeleted===false) {
-                createSpacialtiesIds.push(s.specialtiesId)
-               }
-        })
-    }
 
-    
-
-const doctorSpecialitiesCreateData= createSpacialtiesIds.map((specialties)=>{
-    return {
-        doctorId:doctorInfo.id,
-        specialtiesId:specialties
-    }
-})
-
-
-  const createDoctorSpecialities = await transactionClient.doctorSpecialties.createMany({
-    data:doctorSpecialitiesCreateData
-  })
-
-  const deleteDoctorSpecialities = await transactionClient.doctorSpecialties.deleteMany({
-   where:{
-    specialtiesId:{
-        in:deleteSpacialtiesIds
-    }
-   }
-  })
-
-
-
-
-  return updateDoctorData
-})
-
-
-
-
-return result
+    return result
 }
 
 
