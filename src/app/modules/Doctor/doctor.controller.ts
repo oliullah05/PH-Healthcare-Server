43c2e750b0b0108a8d49cb8eee3ae8b53ec373c9
oliullah05 <mdolihasan@gmail.com>
2024-04-16 23:12:42 +0600

    module 33 starter pack

--- src/app/modules/Doctor/doctor.controller.ts
+++ src/app/modules/Doctor/doctor.controller.ts
@@ -1,25 +1,77 @@
-import httpStatus from "http-status";
-import catchAsync from "../../../shared/catchAsync";
-import sendResponse from "../../../shared/sendResponse";
-import { DoctorServices } from "./doctor.service";
-
-const updateDoctor = catchAsync(async(req,res)=>{
-        const {id}= req.params
-        const user = req.user;
-        const result = await DoctorServices.updateDoctor(id, req.body);
-    
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import { doctorFilterableFields } from './doctor.const';
+import { DoctorServices } from './doctor.service';
 
 
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            message: "Doctor updated successfully",
-            success: true,
-            data: result
-        })
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, doctorFilterableFields);
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await DoctorServices.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Doctors retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
+});
+
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await DoctorServices.getByIdFromDB(id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Doctor retrieval successfully',
+        data: result,
+    });
+});
+
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+    const result = await DoctorServices.updateIntoDB(id, req.body);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Doctor data updated!",
+        data: result
     })
+});
+
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await DoctorServices.deleteFromDB(id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Doctor deleted successfully',
+        data: result,
+    });
+});
 
 
+const softDelete = catchAsync(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const result = await DoctorServices.softDelete(id);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Doctor soft deleted successfully',
+        data: result,
+    });
+});
 
-    export const DoctorControllers = {
-        updateDoctor
-    }
+
+export const DoctorControllers = {
+    updateIntoDB,
+    getAllFromDB,
+    getByIdFromDB,
+    deleteFromDB,
+    softDelete
+}
--- src/app/modules/Doctor/doctor.route.ts
+++ src/app/modules/Doctor/doctor.route.ts
@@ -1,6 +1,36 @@
-import express from "express"
-import { DoctorControllers } from "./doctor.controller";
+import express from 'express'
+import {UserRole } from '@prisma/client';
+import { DoctorValidations } from './doctor.validation';
+import auth from '../../middlewars/auth';
+import validateRequest from '../../middlewars/validateRequest';
+import { DoctorControllers } from './doctor.controller';
 
 const router = express.Router();
-router.patch("/:id",DoctorControllers.updateDoctor)
+
+// task 3
+router.get('/', DoctorControllers.getAllFromDB);
+
+//task 4
+router.get('/:id', DoctorControllers.getByIdFromDB);
+
+router.patch(
+    '/:id',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+    validateRequest(DoctorValidations.update),
+    DoctorControllers.updateIntoDB
+);
+
+//task 5
+router.delete(
+    '/:id',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    DoctorControllers.deleteFromDB
+);
+
+// task 6
+router.delete(
+    '/soft/:id',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+    DoctorControllers.softDelete);
+
 export const DoctorRoutes = router
