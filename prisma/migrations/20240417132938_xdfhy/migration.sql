d7e14a733e62a3d52a7b9be8de63d074fffacda2
oliullah05 <mdolihasan@gmail.com>
2024-04-17 19:29:58 +0600

    Creating Models For Schedule And DoctorSchedule

---
+++ prisma/migrations/20240417132938_xdfhy/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "Schedule" (
+    "id" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Schedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctor_schedules" (
+    "doctorId" TEXT NOT NULL,
+    "scheduleId" TEXT NOT NULL,
+    "isbooked" BOOLEAN NOT NULL,
+    "appointmentId" TEXT,
+
+    CONSTRAINT "doctor_schedules_pkey" PRIMARY KEY ("doctorId","scheduleId")
+);
+
+-- AddForeignKey
+ALTER TABLE "doctor_schedules" ADD CONSTRAINT "doctor_schedules_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_schedules" ADD CONSTRAINT "doctor_schedules_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "Schedule"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
