58717c665682938570e250538807b4794dd64eef
oliullah05 <mdolihasan@gmail.com>
2024-04-17 23:30:13 +0600

    Creating Schedule: Part 2

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,5 +1,27 @@
+import { addHours, format } from "date-fns";
+
 const insertIntoDB = async (payload: any) => {
-    console.log(payload);
+    const {startDate , endDate,startTime,endTime}=payload;
+    const currentDate = new Date(startDate)
+    const lastDate = new Date(endDate)
+
+
+   while(currentDate<=lastDate){
+   const startDateTime = new Date(
+    addHours(
+        `$${format(currentDate,"yyyy-MM-dd")}`,
+        Number(startTime.split(":")[0])
+    )
+   )
+
+   const endDateTime = new Date(
+    addHours(
+        `$${format(lastDate,"yyyy-MM-dd")}`,
+        Number(endTime.split(":")[0])
+    )
+   )
+   console.log(startDateTime,endDateTime);
+   }
 }
 
 
