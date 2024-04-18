8ba9b2ddd45509fd9b75fdfa48a66ac865808b39
oliullah05 <mdolihasan@gmail.com>
2024-04-18 15:28:30 +0600

    34-1 Creating Schedule: Part 3

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,27 +1,48 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
 
 const insertIntoDB = async (payload: any) => {
-    const {startDate , endDate,startTime,endTime}=payload;
+    const { startDate, endDate, startTime, endTime } = payload;
+
     const currentDate = new Date(startDate)
     const lastDate = new Date(endDate)
 
 
-   while(currentDate<=lastDate){
-   const startDateTime = new Date(
-    addHours(
-        `$${format(currentDate,"yyyy-MM-dd")}`,
-        Number(startTime.split(":")[0])
-    )
-   )
+    const intervalTime = 30;
 
-   const endDateTime = new Date(
-    addHours(
-        `$${format(lastDate,"yyyy-MM-dd")}`,
-        Number(endTime.split(":")[0])
-    )
-   )
-   console.log(startDateTime,endDateTime);
-   }
+
+    while (currentDate <= lastDate) {
+
+        // 
+        const startDateTime = new Date(
+            addHours(
+                `$${format(currentDate, "yyyy-MM-dd")}`,
+                Number(startTime.split(":")[0])
+            )
+        )
+
+        // 
+        const endDateTime = new Date(
+            addHours(
+                `$${format(currentDate, "yyyy-MM-dd")}`,
+                Number(endTime.split(":")[0])
+            )
+        )
+
+        while (startDateTime < endDateTime) {
+            const scheduleData = {
+                startDateTime,
+                endDateTime: addMinutes(startDateTime, Number(intervalTime))
+            }
+            console.log({scheduleData});
+        }
+
+
+
+
+
+
+
+    }
 }
 
 
